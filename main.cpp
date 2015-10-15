3c7c81cf5a77ecd951299d898f0d53aff03953f3
Linh Nguyen <nglinh2312@gmai.com>
2015-10-15 18:07:14 +0000

    removing namespaces, use explicit calls instead

--- main.cpp
+++ main.cpp
@@ -5,30 +5,52 @@
 #include <alpr.h>
 #include <unistd.h>
 #include <ctime>
+#include <thread>
 #include "RaspiCamCV.h"
 
 #include <iostream>
 #include <stdio.h>
 
-using namespace std;
-using namespace cv;
-
-
-/** Function Headers */
-std::vector<Rect> detectByCascade( Mat frame );
-
-void detectByCascadeAndKalmanFilter( Mat frame, map<int,KalmanFilter>& KFm);
-
-void bgSub(Ptr<BackgroundSubtractor> bg, SimpleBlobDetector& detector, Mat& frame);
-
-void bgSubAndKalmanFilter(BackgroundSubtractorMOG2& bg, SimpleBlobDetector& detector, Mat& frame, map<int,KalmanFilter>& KFm);
-
 /** Global variables */
-String car_cascade_name = "../classifier/lbpcars1.xml";
-String input_file, output_file;
-CascadeClassifier car_cascade;
-string window_name = "Capture - Car detection";
-RNG rng(12345);
+std::string car_cascade_name = "../classifier/carsg.xml";
+std::string input_file, output_file;
+cv::CascadeClassifier car_cascade;
+std::string window_name = "Capture - Car detection";
+int const NUM_THREAD = 4;
+
+void detectCars(cv::VideoCapture video_capture, RaspiCamCvCapture* capture, bool video_mode, int* nCount) {
+	std::vector<cv::Rect> cars;
+	cv::Mat frame, mask, canvas;
+	char c;
+	double scaleUp = 6.25;
+    double scaleDown = 0.16;
+	while(!(video_mode && !video_capture.isOpened())) { 
+		if(video_mode) {
+			video_capture.read(frame);
+			cv::cvtColor(frame, canvas, CV_BGR2GRAY);
+		}
+		else {
+			frame = cv::cvarrToMat(raspiCamCvQueryFrame(capture));
+			canvas = frame;
+		}
+		cv::resize(canvas, mask, cv::Size(), scaleDown, scaleDown);
+		(*nCount)++;
+		double car_min_pct = 0.05;
+		double car_max_pct = 0.3;
+		//cout << "Image size: " << mask.size() <<endl;
+		int car_min_size = mask.size().height*car_min_pct;
+		int car_max_size = mask.size().height*car_max_pct;
+		//-- Detect cars
+		car_cascade.detectMultiScale( mask, cars, 1.2, 4,0, cv::Size(car_min_size, car_min_size), cv::Size(car_max_size, car_max_size));
+		if (cars.size()){
+			std::cout << "detected " << cars.size() << std::endl;
+		}
+		int c = cv::waitKey(1);
+        if( (char)c == 'q' )
+			 break;
+	}
+	return;
+}
 
 int openalprDemo() {
     // Initialize the library using United States style license plates.  
@@ -42,7 +64,7 @@ int openalprDemo() {
     // comparing the plate text with the regional pattern.
     openalpr.setDefaultRegion("sg");
 
-    // Make sure the library loaded before continuing.  
+	// Make sure the library loaded before continuing.  
     // For example, it could fail if the config/runtime_data is not found
     if (openalpr.isLoaded() == false)
     {
@@ -70,41 +92,39 @@ int openalprDemo() {
 }
 
 //detect and draw bounding box by backgroun subtraction
-void bgSub(Ptr<BackgroundSubtractor> bg, SimpleBlobDetector& detector, Mat& frame){
+void bgSub(cv::Ptr<cv::BackgroundSubtractor> bg, cv::SimpleBlobDetector& detector, cv::Mat& frame){
     // Mat back;
-    Mat fore;
-    Mat frame_gray;
+    cv::Mat fore;
+    cv::Mat frame_gray;
+    
+	std::vector<std::vector<cv::Point> > contours;
+    std::vector<cv::KeyPoint> keypoints;
 
-
-
-    vector<vector<Point> > contours;
-    vector<KeyPoint> keypoints;
-
-    namedWindow("Frame");
+    cv::namedWindow("Frame");
     // namedWindow("Background");
-    cvtColor(frame,frame_gray,CV_BGR2GRAY);
+    cv::cvtColor(frame,frame_gray,CV_BGR2GRAY);
     // imshow("gray", frame_gray);
     bg->apply(frame_gray,fore);
     // bg.getBackgroundImage(back);
     
-    GaussianBlur( fore, fore, Size( 3, 3), 1,1);
+    cv::GaussianBlur( fore, fore, cv::Size( 3, 3), 1,1);
     // threshold(fore,fore,100,255,THRESH_TRUNC);
     // Canny(fore,fore,30,90,3);
 
     
     // detector.detect(fore,keypoints);
-    erode(fore,fore,Mat());
-    dilate(fore,fore,Mat());
+	cv::erode(fore, fore, cv::Mat());
+    cv::dilate(fore, fore, cv::Mat());
     
-    findContours(fore,contours,CV_RETR_EXTERNAL,CV_CHAIN_APPROX_NONE);
-    // drawContours(frame_gray,contours,-1,Scalar(0,0,255),2);
+    cv::findContours(fore,contours,CV_RETR_EXTERNAL,CV_CHAIN_APPROX_NONE);
+    // drawContours(frame_gray,contours,-1,Scalar(0,0,255),2);    for (int i = 0; i < contours.size(); i++){
     for (int i = 0; i < contours.size(); i++){
-        vector<Point> contours_poly;
-        approxPolyDP( contours[i], contours_poly, 3 , true);
-        Rect boundRect = boundingRect(contours_poly);
-        if (norm(boundRect.tl() - boundRect.br()) < 30)
+        std::vector<cv::Point> contours_poly;
+        cv::approxPolyDP( contours[i], contours_poly, 3 , true);
+        cv::Rect boundRect = cv::boundingRect(contours_poly);
+        if (cv::norm(boundRect.tl() - boundRect.br()) < 30)
             continue;
-        rectangle(frame, boundRect.tl(), boundRect.br(), Scalar(255,0,255));
+        cv::rectangle(frame, boundRect.tl(), boundRect.br(), cv::Scalar(255,0,255));
     }
     // drawKeypoints( frame, keypoints, frame, Scalar(0,0,255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
     // for (int i = 0; i < keypoints.size(); i++){
@@ -114,37 +134,26 @@ void bgSub(Ptr<BackgroundSubtractor> bg, SimpleBlobDetector& detector, Mat& fram
     return;
 }
 
-//detect and display bounding box by haar cascade classifier
-std::vector<Rect> detectByCascade( Mat frame )
-{
-    std::vector<Rect> cars;
-    Mat frame_gray;
-    
-    cvtColor( frame, frame_gray, CV_BGR2GRAY );
-    
-    //-- Detect cars
-    car_cascade.detectMultiScale( frame_gray, cars, 1.1, 6,0, Size(200,200), Size(300,300));
-    return cars;
-}
 
+//detect and display bounding box by haar cascade classifier
 int main( int argc, const char** argv )
 {
-    std::vector<Rect> cars;
+    std::vector<cv::Rect> cars;
 	time_t begin, end;
     int nCount = 0;
 	RaspiCamCvCapture * capture;
-	VideoCapture video_input;
-	VideoWriter output;
-	bool output_video = false;			
-    Mat frame;
+	cv::VideoCapture video_input;
+	cv::VideoWriter output;
+	bool video_mode = false;			
+    cv::Mat frame;
     if (argc > 1)
     {
         video_input.open(argv[1]);
-		output.open(output_file, CV_FOURCC('F','M','P','4'), 14.999, Size(320,240));
-		output_video = true;
+		//output.open(output_file, CV_FOURCC('F','M','P','4'), 14.999, Size(320,240));
+		video_mode = true;
     }
 	else {
-		cout << "using camera" <<endl;
+		std::cout << "using camera" << std::endl;
 		RASPIVID_CONFIG * config = (RASPIVID_CONFIG*)malloc(sizeof(RASPIVID_CONFIG));
 		config->width=1280;
 		config->height=720;
@@ -155,45 +164,24 @@ int main( int argc, const char** argv )
 		free(config);
 	}
 
-    if( !car_cascade.load( car_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
-    double scaleUp = 1;
-    double scaleDown = 1;
-	namedWindow("VideoCaptureTest");
+    if( !car_cascade.load( car_cascade_name ) ){ 
+		std::cout<< "--(!)Error loading" << std::endl; 
+		return -1; 
+	}
+	cv::namedWindow("VideoCaptureTest");
 	time( &begin);
-    while( true ){
-		//IplImage* image = raspiCamCvQueryFrame(capture);
-		frame = cvarrToMat(raspiCamCvQueryFrame(capture));
-        // float scale_w = frame.cols/160;
-        // float scale_h = frame.rows/120;
-        if (!frame.empty()) {
-			//Mat canvas;                    
-			//resize(frame, canvas, Size(), scaleDown, scaleDown);
-            //cars = detectByCascade(canvas);
-            // detectByCascadeAndKalmanFilter(frame, KFm);
-            //for (int i = 0; i < cars.size(); i++){
-				// rectangle(frame, cars[i].tl()*scaleUp, cars[i].br()*scaleUp, Scalar(255,0,255), 3);
-				// rectangle(canvas, cars[i], Scalar(255,0,255));
-				// Mat ROI(frame, cars[i]);
-                // imshow("ROI", ROI);
-			    // cvtColor(ROI, ROI_hsv, CV_BGR2HSV);
-                // calcHist(&ROI_hsv, 1, channels, Mat(), hist, 2, histSize, ranges, true, false);
-            //}
-        //}
-		nCount++;
-        //imshow("VideoCaptureTest", frame);
-		if(output_video)
-	        output << frame;
-			int c = waitKey(1);
-            if( (char)c == 'q' ) { break; }
-        }
-        else {
-            cout << "empty frame\n";
-            exit(0);
-        }
-    }
+	std::thread pool[NUM_THREAD];
+	for (int i = 0; i < NUM_THREAD; i++) {
+		pool[i] = std::thread(detectCars, video_input, capture, video_mode, &nCount);
+	}
+	std::cout << "All threads started" << std::endl;
+	for (int i = 0; i < NUM_THREAD; i++) {
+		pool[i].join();
+		std::cout << "Thread " << i << "joined" <<std::endl;
+	}
 	time( &end);
 	double elapsed = difftime(end, begin);
-	cout << elapsed << " FPS = " << (float) ((float) (nCount) /elapsed) <<endl;
+	//std::cout << elapsed << " FPS = " << (float) ((float) (nCount) /elapsed) << std::endl;
     return 0;
 }
 
