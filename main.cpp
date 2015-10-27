b02ff8dc330c40e24cf4d721c0f7f89af85cb806
Linh Nguyen <nglinh2312@gmai.com>
2015-10-27 19:13:36 +0000

    added POCO in CMakeList and removed detection time in post since that can be recovered in Mongo

--- main.cpp
+++ main.cpp
@@ -7,20 +7,52 @@
 #include <ctime>
 #include <thread>
 #include "RaspiCamCV.h"
+#include <Poco/Net/HTTPClientSession.h>
+#include <Poco/Net/HTTPRequest.h>
+#include <Poco/Net/HTTPResponse.h>
+#include <Poco/Path.h>
+#include <Poco/URI.h>
+#include <Poco/Exception.h>
 
 #include <iostream>
 #include <stdio.h>
+#include <stdlib.h>
 
 /** Global variables */
-std::string car_cascade_name = "../classifier/carsg.xml";
+std::string car_cascade_name = "../classifier/lbpcars1.xml";
 std::string input_file, output_file;
 cv::CascadeClassifier car_cascade;
 std::string window_name = "Capture - Car detection";
 int const NUM_THREAD = 4;
 
-void detectCars(cv::VideoCapture video_capture, RaspiCamCvCapture* capture, bool video_mode, int* nCount) {
+void detectCars(cv::VideoCapture& video_capture, RaspiCamCvCapture* capture, bool video_mode) {
+	alpr::Alpr openalpr("sg", "/home/pi/openalpr/config/openalpr.conf.in", "/home/pi/openalpr/runtime_data");
+	openalpr.setDefaultRegion("sg");	
+	if (!openalpr.isLoaded()){
+		std::cerr << "Error loading OpenALPR" << std::endl;
+		return;
+	}
+
+	std::string server_uri = std::getenv("TRAFFIC_EYE_URI");
+	std::string rpi_id = std::getenv("RPI_ID");
+	std::cout << rpi_id << std::endl;
+	server_uri += rpi_id.substr(1);
+	std::cout << server_uri << std::endl;
+
+	//prepare uri
+	Poco::URI uri(server_uri);
+
+	//prepare session
+	Poco::Net::HTTPClientSession session(uri.getHost(), uri.getPort());
+	session.setKeepAlive(true);
+	
+	//prepare path
+	std::string path(uri.getPathAndQuery());
+	if(path.empty()) path = "/";
+
+
 	std::vector<cv::Rect> cars;
-	cv::Mat frame, mask, canvas;
+	cv::Mat frame, mask, canvas, bgr_mask;
 	char c;
 	double scaleUp = 6.25;
     double scaleDown = 0.16;
@@ -31,64 +63,72 @@ void detectCars(cv::VideoCapture video_capture, RaspiCamCvCapture* capture, bool
 		}
 		else {
 			frame = cv::cvarrToMat(raspiCamCvQueryFrame(capture));
+			cv::cvtColor(frame, frame, CV_GRAY2BGR);	//assuming it is captured in monochrome mode
 			canvas = frame;
 		}
+		
 		cv::resize(canvas, mask, cv::Size(), scaleDown, scaleDown);
-		(*nCount)++;
-		double car_min_pct = 0.05;
-		double car_max_pct = 0.3;
+		
+		//Detect plates directly	
+		/*std::vector<alpr::AlprRegionOfInterest> masks;
+		masks.push_back(alpr::AlprRegionOfInterest(0,0,frame.size().width, frame.size().height));
+		alpr::AlprResults results = openalpr.recognize(frame.data, frame.elemSize(), frame.size().width, frame.size().height, masks);
+		for (int i = 0; i < results.plates.size(); i++) {
+			alpr::AlprPlateResult plate = results.plates[i];
+			alpr::AlprPlate candidate = plate.topNPlates[0];	//getting only the top result
+			std::cout << "    - " << candidate.characters << "\t confidence: " << candidate.overall_confidence;
+			std::cout << "\t pattern_match: " << candidate.matches_template << std::endl;
+			Poco::Net::HTTPRequest req(Poco::Net::HTTPRequest::HTTP_POST, uri.getPathAndQuery(), Poco::Net::HTTPMessage::HTTP_1_1);
+			req.setContentType("application/x-www-form-urlencoded");
+			req.setKeepAlive(true);
+			time_t detection_time = time(0);
+			std::ostringstream convert;
+			convert << detection_time;
+			std::string reqBody("plate_id=" + candidate.characters + "&detection_time=" + convert.str());
+			req.setContentLength( reqBody.length() );
+			std::ostream& stream = session.sendRequest(req);
+			stream << reqBody;
+			req.write(std::cout);
+		}*/
+
+		//Detect car first, then detect plate
+		double car_min_pct = 0.1;
+		double car_max_pct = 0.7;
 		//cout << "Image size: " << mask.size() <<endl;
 		int car_min_size = mask.size().height*car_min_pct;
 		int car_max_size = mask.size().height*car_max_pct;
 		//-- Detect cars
-		car_cascade.detectMultiScale( mask, cars, 1.2, 4,0, cv::Size(car_min_size, car_min_size), cv::Size(car_max_size, car_max_size));
+		car_cascade.detectMultiScale( mask, cars, 1.1, 5,0, cv::Size(car_min_size, car_min_size), cv::Size(car_max_size, car_max_size));
 		if (cars.size()){
 			std::cout << "detected " << cars.size() << std::endl;
+			std::vector<alpr::AlprRegionOfInterest> masks;
+			for (int i = 0; i < cars.size() ; i++) {
+				masks.push_back(alpr::AlprRegionOfInterest((int) cars[i].tl().x*scaleUp, (int) cars[i].tl().y*scaleUp, (int) cars[i].size().width*scaleUp, (int) cars[i].size().height*scaleUp));
+				//cv::rectangle(frame, cars[i].tl() * scaleUp, cars[i].br() * scaleUp, cv::Scalar(0,0,255), 3);
+			}
+			//cv::cvtColor(frame, bgr_mask, CV_GRAY2BGR);
+			alpr::AlprResults results = openalpr.recognize(frame.data, frame.elemSize(), frame.size().width, frame.size().height, masks);
+			for (int i = 0; i < results.plates.size(); i++) {
+				alpr::AlprPlateResult plate = results.plates[i];
+				alpr::AlprPlate candidate = plate.topNPlates[0];	//getting only the top result
+				std::cout << "    - " << candidate.characters << "\t confidence: " << candidate.overall_confidence;
+				std::cout << "\t pattern_match: " << candidate.matches_template << std::endl;
+				Poco::Net::HTTPRequest req(Poco::Net::HTTPRequest::HTTP_POST, uri.getPathAndQuery(), Poco::Net::HTTPMessage::HTTP_1_1);
+				req.setContentType("application/x-www-form-urlencoded");
+				req.setKeepAlive(true);
+				std::string reqBody("plate_id=" + candidate.characters); 
+				req.setContentLength( reqBody.length() );
+				std::ostream& stream = session.sendRequest(req);
+				stream << reqBody;
+				req.write(std::cout);
+			}	
 		}
+		//cv::imshow(window_name, frame);
 		int c = cv::waitKey(1);
-        if( (char)c == 'q' )
-			 break;
+		if( (char)c == 'q' )
+			break;
 	}
 	return;
-}
-
-int openalprDemo() {
-    // Initialize the library using United States style license plates.  
-    // You can use other countries/regions as well (for example: "eu", "au", or "kr")
-    alpr::Alpr openalpr("sg", "~/openalpr/config/openalpr.conf.in", "~/openalpr/runtime_data");
-
-    // Optionally specify the top N possible plates to return (with confidences).  Default is 10
-    openalpr.setTopN(20);
-
-    // Optionally, provide the library with a region for pattern matching.  This improves accuracy by 
-    // comparing the plate text with the regional pattern.
-    openalpr.setDefaultRegion("sg");
-
-	// Make sure the library loaded before continuing.  
-    // For example, it could fail if the config/runtime_data is not found
-    if (openalpr.isLoaded() == false)
-    {
-        std::cerr << "Error loading OpenALPR" << std::endl;
-        return 1;
-    }
-
-    // Recognize an image file.  You could alternatively provide the image bytes in-memory.
-    alpr::AlprResults results = openalpr.recognize("/path/to/image.jpg");
-
-    // Iterate through the results.  There may be multiple plates in an image, 
-    // and each plate return sthe top N candidates.
-    for (int i = 0; i < results.plates.size(); i++)
-    {
-      alpr::AlprPlateResult plate = results.plates[i];
-      std::cout << "plate" << i << ": " << plate.topNPlates.size() << " results" << std::endl;
-
-        for (int k = 0; k < plate.topNPlates.size(); k++)
-        {
-          alpr::AlprPlate candidate = plate.topNPlates[k];
-          std::cout << "    - " << candidate.characters << "\t confidence: " << candidate.overall_confidence;
-          std::cout << "\t pattern_match: " << candidate.matches_template << std::endl;
-        }
-    }
 }
 
 //detect and draw bounding box by backgroun subtraction
@@ -148,6 +188,7 @@ int main( int argc, const char** argv )
     cv::Mat frame;
     if (argc > 1)
     {
+		std::cout << "video mode" << std::endl;
         video_input.open(argv[1]);
 		//output.open(output_file, CV_FOURCC('F','M','P','4'), 14.999, Size(320,240));
 		video_mode = true;
@@ -158,7 +199,7 @@ int main( int argc, const char** argv )
 		config->width=1280;
 		config->height=720;
 		config->bitrate=0;	// zero: leave as default
-		config->framerate=60;
+		config->framerate=49;
 		config->monochrome=1;
 		capture = (RaspiCamCvCapture *) raspiCamCvCreateCameraCapture2(0, config); 
 		free(config);
@@ -168,20 +209,24 @@ int main( int argc, const char** argv )
 		std::cout<< "--(!)Error loading" << std::endl; 
 		return -1; 
 	}
-	cv::namedWindow("VideoCaptureTest");
+
+	//cv::namedWindow(window_name);
 	time( &begin);
 	std::thread pool[NUM_THREAD];
 	for (int i = 0; i < NUM_THREAD; i++) {
-		pool[i] = std::thread(detectCars, video_input, capture, video_mode, &nCount);
+		pool[i] = std::thread(detectCars, std::ref(video_input), capture, video_mode);
 	}
 	std::cout << "All threads started" << std::endl;
 	for (int i = 0; i < NUM_THREAD; i++) {
 		pool[i].join();
 		std::cout << "Thread " << i << "joined" <<std::endl;
 	}
+	//detectCars(video_input, capture, video_mode);
 	time( &end);
 	double elapsed = difftime(end, begin);
 	//std::cout << elapsed << " FPS = " << (float) ((float) (nCount) /elapsed) << std::endl;
+	if (!video_mode) {
+		raspiCamCvReleaseCapture(&capture);
+	}
     return 0;
 }
-
